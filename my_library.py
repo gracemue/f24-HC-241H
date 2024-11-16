3ef9f5c9fa1ea9754ac958a64d303d707d79c834
gracemue <gracemue@uoregon.edu>
2024-11-16 12:42:06 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,2 +1,32 @@
 def test_load():
   return 'loaded'
+
+def compute_probs(neg,pos):
+  p0 = neg/(neg+pos)
+  p1 = pos/(neg+pos)
+  return [p0,p1]
+
+def cond_prob(full_table, the_evidence_column, the_evidence_column_value, the_target_column, the_target_column_value):
+  assert the_evidence_column in full_table
+  assert the_target_column in full_table
+  assert the_evidence_column_value in up_get_column(full_table, the_evidence_column)
+  assert the_target_column_value in up_get_column(full_table, the_target_column)
+
+def cond_probs_product(full_table, evidence_row, target_column, target_column_value):
+  assert target_column in full_table
+  assert target_column_value in up_get_column(full_table, target_column)
+  assert isinstance(evidence_row, list)
+  assert len(evidence_row) == len(up_list_column_names(full_table)) - 1
+
+def prior_prob(full_table, the_column, the_column_value):
+  assert the_column in full_table
+  assert the_column_value in up_get_column(full_table, the_column)
+  #your function body below
+  t_list = up_get_column(full_table, the_column)
+  p_a = sum([1 if v==the_column_value else 0 for v in t_list])/len(t_list)
+  return p_a
+
+def naive_bayes(full_table, evidence_row, target_column):
+  assert target_column in full_table
+  assert isinstance(evidence_row, list)
+  assert len(evidence_row) == len(up_list_column_names(full_table)) - 1
