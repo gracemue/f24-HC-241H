b50675920b6a1890a029e8ca498280bc4147c0ef
gracemue <gracemue@uoregon.edu>
2024-11-16 12:52:11 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -1,16 +1,15 @@
 def test_load():
   return 'loaded'
 
-def compute_probs(x,y):
+def compute_probs(x, y):
   p0 = x/(x+y)
   p1 = y/(x+y)
   return [p0,p1]
 
-def cond_prob(full_table, the_evidence_column, the_evidence_column_value, the_target_column, the_target_column_value):
-  assert the_evidence_column in full_table
-  assert the_target_column in full_table
-  assert the_evidence_column_value in up_get_column(full_table, the_evidence_column)
-  assert the_target_column_value in up_get_column(full_table, the_target_column)
+def cond_prob(table, column, column_value, target_column, target_column_value):
+  assert column in table
+  assert target_column in table
+  assert column_value in up_get_column(table, column)
 
 def cond_probs_product(full_table, evidence_row, target_column, target_column_value):
   assert target_column in full_table
@@ -21,10 +20,7 @@ def cond_probs_product(full_table, evidence_row, target_column, target_column_va
 def prior_prob(full_table, the_column, the_column_value):
   assert the_column in full_table
   assert the_column_value in up_get_column(full_table, the_column)
-  #your function body below
-  t_list = up_get_column(full_table, the_column)
-  p_a = sum([1 if v==the_column_value else 0 for v in t_list])/len(t_list)
-  return p_a
+
 
 def naive_bayes(full_table, evidence_row, target_column):
   assert target_column in full_table
