031d717774306a756db07784bff546df5e4fe095
gracemue <gracemue@uoregon.edu>
2024-11-18 10:40:44 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -6,10 +6,11 @@ def compute_probs(x, y):
   p1 = y/(x+y)
   return [p0,p1]
 
-def cond_prob(table, column, column_value, target_column, target_column_value):
-  assert column in table
-  assert target_column in table
-  assert column_value in up_get_column(table, column)
+def cond_prob(table, evidence, evidence_value, target, target_value):
+  t_subset = up_table_subset(table, target, 'equals', target_value)
+  e_list = up_get_column(t_subset, evidence)
+  p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
+  return p_b_a + .01  #Laplace smoothing factor
 
 def cond_probs_product(full_table, evidence_row, target_column, target_column_value):
   assert target_column in full_table
